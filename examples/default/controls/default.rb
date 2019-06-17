079fce930cbff9b858ce7e27ea79a1686af82a20
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-06-17 16:04:20 -0400

    Added inspec profile for examples

---
+++ examples/default/controls/default.rb
@@ -0,0 +1,9 @@
+#
+# inspec_profile::azuread-service-principal-default
+# controls::default
+#
+# author::cloudsquad
+# description:: Inspec controls for azuread-service-principal-default
+#
+
+include_controls 'azuread-service-principal'
