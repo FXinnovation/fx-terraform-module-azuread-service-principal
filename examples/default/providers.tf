dc31991c271d246714e7f8a2b6e9f3565d4766db
Christophe van de Kerchove <christophe.vkerchove@fxinnovation.com>
2019-07-26 13:35:35 -0400

    Add provider

--- examples/default/providers.tf
+++ examples/default/providers.tf
@@ -6,6 +6,14 @@ provider "azuread" {
   tenant_id       = "${var.tenant_id}"
 }
 
+provider "azurerm" {
+  version         = "~> 1.28.0"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+}
+
 provider "random" {
   version = "~> 2.0"
 }
